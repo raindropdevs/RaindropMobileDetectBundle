217a5ba32e79b62350525ab50d1bb29bce62da76
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-05-08 15:14:39 +0200

    add mobile redirect listener

--- EventListener/RequestListener.php
+++ EventListener/RequestListener.php
@@ -4,6 +4,7 @@ namespace Raindrop\MobileDetectBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 use Raindrop\MobileDetectBundle\ActiveDevice;
 use Raindrop\MobileDetectBundle\DeviceDetection\DeviceDetectionInterface;
@@ -13,6 +14,11 @@ use Raindrop\MobileDetectBundle\DeviceDetection\DeviceDetectionInterface;
  */
 class RequestListener
 {
+    CONST REDIRECT = 'redirect';
+    CONST NO_REDIRECT = 'no_redirect';
+    CONST REDIRECT_WITHOUT_PATH = 'redirect_without_path';
+    CONST MOBILE = 'mobile';
+
     /**
      * @var ActiveDevice $activeDevice
      */
@@ -47,11 +53,13 @@ class RequestListener
         $this->deviceDetection = $deviceDetection;
     }
 
-    /**
+   /**
     * @param GetResponseEvent $event
     */
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $request = $event->getRequest();
+
         if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()) {
             $cookieValue = null;
 
@@ -62,7 +70,73 @@ class RequestListener
             if ($cookieValue && $cookieValue !== $this->activeDevice->getName()) {
                 $this->activeDevice->setName($cookieValue);
             }
+
+            // Redirects to the mobile version
+            if ($this->hasMobileRedirect($request)) {
+                if (($response = $this->getMobileRedirectResponse($request))) {
+                    $event->setResponse($response);
+                }
+
+                return;
+            }
         }
     }
 
+   /**
+    * Detects mobile redirections.
+    *
+    * @param Request $request
+    * @return boolean
+    */
+    private function hasMobileRedirect($request)
+    {
+        if (!$this->redirectConf['mobile']['is_enabled']) {
+            return false;
+        }
+
+        $isMobile = $this->deviceDetection->isMobile();
+        $isMobileHost = ($this->getCurrentHost($request) === $this->redirectConf['mobile']['host']);
+
+        if ($isMobile && !$isMobileHost) {
+            return true;
+        }
+
+        return false;
+    }
+
+   /**
+    * Gets the mobile RedirectResponse.
+    *
+    * @return \Symfony\Component\HttpFoundation\RedirectResponse
+    */
+    private function getMobileRedirectResponse($request)
+    {
+        if (($host = $this->getRedirectUrl(self::MOBILE, $request))) {
+            return new RedirectResponse($host);
+        }
+    }
+
+   /**
+    * Gets the redirect url.
+    *
+    * @param string  $platform
+    * @param Request $request
+    *
+    * @return string
+    */
+    private function getRedirectUrl($platform, $request)
+    {
+        return $request->getScheme() . '://' . $this->redirectConf[$platform]['host'];
+    }
+
+   /**
+    * Gets the current host.
+    *
+    * @param Request $request
+    * @return string
+    */
+    private function getCurrentHost($request)
+    {
+        return $request->getScheme() . '://' . $request->getHost();
+    }
 }
